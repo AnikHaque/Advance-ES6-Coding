f9813d3e14817ae7836f4d596b0a1eef6098b053
EkramulHaqueEngineer <ekramulhaqueengineer@gmail.com>
2021-08-25 00:53:05 +0600

    first commit

---
+++ destructuring.js
@@ -0,0 +1,12 @@
+const daffodil ={
+    department: {first: 'CSE', second: 'EEE'}, 
+    Students: 12000, 
+    stuffs: 48
+}
+
+const {first,second} = daffodil.department;
+console.log(first,second);
+
+// console.log(daffodil.Students);
+// const {department} = daffodil;
+// console.log(department);
